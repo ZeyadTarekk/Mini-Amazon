08722b071b5ba9863ce629fee380f066f9cb89ca
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-01 23:18:20 +0200

    add basic styling (inline)

--- routes/shop.js
+++ routes/shop.js
@@ -5,10 +5,7 @@ const path = require("path");
 const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  console.log("Giving back");
   res.sendFile(path.join(rootDir, "views", "shop.html"));
-  // res.end();
-  console.log("Giving back done");
 });
 
 module.exports = router;
