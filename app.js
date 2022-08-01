269cb8f2915a0f0bb3154614f1b1fcefe01e47c1
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-01 16:28:53 +0200

    adding 404 page

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
