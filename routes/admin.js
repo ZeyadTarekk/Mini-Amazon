e5719a55b441ba13a500ffd08c5e34f10e934fe0
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-01 19:43:23 +0200

    adding utility path

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 // admin/add-product
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // admin/product
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
+const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
   console.log("Giving back");
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
   // res.end();
   console.log("Giving back done");
 });
