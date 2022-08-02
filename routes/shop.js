1abeb8b3f50b8c7e0eb28c33b688cb292d99afed
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-02 22:30:06 +0200

    Looping through products array through pug

--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,6 @@ const rootDir = require("../util/path");
 const adminExports = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log(adminExports.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminExports.products;
   res.render("shop", { prods: products, docTitle: "Shop" });
