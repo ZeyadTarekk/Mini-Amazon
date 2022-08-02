4ab4b1adbff98b6c6711e2b9b7e348c8a3e049b3
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-02 12:56:24 +0200

    serving css files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // admin/anyRoute
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
