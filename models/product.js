5f8910a57e9892f8ac39d15b85bbebabde661ef6
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-06 17:46:00 +0200

    refactoring model

--- models/product.js
+++ models/product.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
-);
+
+const getPath = () =>
+  path.join(path.dirname(require.main.filename), "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(getPath(), (err, fileContent) => {
+    if (err) callback([]);
+    else callback(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   title;
 
@@ -13,26 +19,15 @@ module.exports = class Product {
   }
 
   save() {
-    console.log(p);
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        console.log(fileContent);
-        products = JSON.parse(fileContent);
-      }
-      products.push(this); //that callback function attached to the readfile must be arrow function to make the this keyword point to the whole class
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+    getProductsFromFile((products) => {
+      fs.writeFile(getPath(), JSON.stringify([...products, this]), (err) => {
+        //that callback function attached to the readfile must be arrow function to make the this keyword point to the whole class
+        if (err) console.log(err);
       });
     });
   }
 
   static fetchAll(callback) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      }
-      callback(JSON.parse(fileContent));
-    });
+    getProductsFromFile(callback);
   }
 };
