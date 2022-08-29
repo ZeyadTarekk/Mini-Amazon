ff788bbca36860bd39c5defb5f226b2249396102
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-08-30 00:21:59 +0200

    Creating cart and its relations

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const sequelize = require("./util/database");
 const adminRoutes = require("./routes/admin");
@@ -34,6 +36,10 @@ app.use(errorController.get404);
 // Define releations between models
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
