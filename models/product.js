ba693905de412150f7e0cba2670de7310e0ba0ef
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-04 22:57:56 +0200

    Create product schema

--- models/product.js
+++ models/product.js
@@ -1,3 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
 // const getDb = require("../util/database").getDb;
 // const mongodb = require("mongodb");
 // class Product {
