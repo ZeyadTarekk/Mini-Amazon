a4af1e9c7f5f9f6a6426198d8aece2f0a30a4de5
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-05 12:30:55 +0200

    Fetching all products at the index page

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getIndex = async (req, res, next) => {
-  const products = await Product.fetchAll();
+  const products = await Product.find();
   res.render("shop/index", {
     pageTitle: "Shop",
     prods: products,
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
-// router.get("/", shopController.getIndex);
+router.get("/", shopController.getIndex);
 
 // router.get("/cart", shopController.getCart);
 
