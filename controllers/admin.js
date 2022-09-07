0b54892d060ec05977b0730e69a1d700272bdb6f
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-07 13:48:51 +0200

    First way to protect routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,6 +11,7 @@ exports.getAdminProducts = async (req, res, next) => {
 };
 
 exports.getAddProduct = (req, res, next) => {
+  if (!req.session.isLoggedIn) return res.redirect("/login");
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
