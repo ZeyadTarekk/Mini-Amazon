45074981cb490d935e2ec2f7e6918408aeb024cb
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-07 15:17:39 +0200

    Fix errorMessage flash

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,13 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) message = message[0];
+  else message = null;
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage: req.flash("error"),
+    errorMessage: message,
   });
 };
 
