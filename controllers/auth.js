25ab1cef77bdb57955a376b83d44394309714e2f
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-07 17:43:08 +0200

    Sending mails through the application

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
+
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.API_KEY,
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -77,6 +89,17 @@ exports.postSignup = async (req, res, next) => {
     },
   });
   await newUser.save();
+
+  try {
+    await transporter.sendMail({
+      to: email,
+      from: "zeyad@node.com",
+      subject: "Signup succeeded!",
+      html: "<h1>You Successfully signed up!</h1>",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
   res.redirect("/login");
 };
