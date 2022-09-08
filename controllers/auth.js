69f150c4d7d4cdffa7df41ee870cc2dd27d76cb4
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-08 21:21:19 +0200

    Keep the old data on error

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,6 +73,11 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 };
 
@@ -87,6 +92,11 @@ exports.postSignup = async (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword,
+      },
     });
   }
 
