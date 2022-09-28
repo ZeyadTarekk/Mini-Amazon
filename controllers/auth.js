5599822deaa639d6cc00d1c47fe58ab6a23dac4d
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-09-28 11:15:57 +0200

    rename sendgripd api key

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ const User = require("../models/user");
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key: process.env.API_KEY,
+      api_key: process.env.SENDGRID_API_KEY,
     },
   })
 );
